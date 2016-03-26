a2ac0966a50d8e2b253c6fd654ffb4455bcf2e69
JMartinUSA <waverider.jmart@gmail.com>
2016-03-26 12:55:53 -0400

    Added files via upload

--- encrypterClean.js
+++ encrypterClean.js
@@ -1,7 +1,8 @@
 var key;
 var input;
 var inputLength;
-var symbol = [' ', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '!', '1', '@', '2', '#', '3', '$', '4', '%', '5', '^', '6', '&', '7', '*', '8', '(', '9', ')', '0', '_', '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', '"', "'", '<', ',', '>', '.', '?', '/'];
+var symbol = [' ', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '!', '1', '@', '2', '#', '3', '$', '4', '%', '5', '^', '6', '&', '7', '*', '8', '(', '9', ')', '0', '_', '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', '"', "'", '<', ',', '>', '.', '/', '?','	'];
+var printSymbol = ['&nbsp', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '!', '1', '@', '2', '#', '3', '$', '4', '%', '5', '^', '6', '&', '7', '*', '8', '(', '9', ')', '0', '_', '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', '"', "'", '<', ',', '>', '.', '/', '?','	'];
   
 
     function field(){
@@ -19,6 +20,11 @@ var symbol = [' ', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', '
 				keyDecrypt = keyDecrypt+keyDecrypt
 			}						
 		}*/
+		console.log(key);
+    }
+    
+    function reset(){
+    	document.getElementById("inputTextToBeEncrypted").value="Paste your text here!";
     }
 
 	function Encrypt(){
@@ -30,25 +36,29 @@ var symbol = [' ', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', '
 		while(i <= inputLength){
 			j = 0;
 			//This fires once for every possible character in the key field 
-			while(j < 92){
+			while(j < 93){
 				k = 0;
 				if (key.substring(i,i+1) == symbol[j]){				
 					var jFinal = j;	
 					//This fires once for every 			
-					while(k < 92){										
-						if(input.substring(i,i+1) == symbol[k]){						
+					while(k < 93){
+						 if(input.substring(i,i+1) == symbol[k]){						
 							var kFinal = k;						
 							var output = kFinal + jFinal;						
-							if(output > 91){output=output-92}
-							var symboloutput = symbol[output];
+							if(output > 92){output=output-93}
+							var symboloutput = printSymbol[output];
 							//console.log(symboloutput);
-							document.getElementById("output").innerHTML+=symboloutput;						
-							k = 92;				
+							document.getElementById("output").innerHTML+=symboloutput;
+							output = 0;
+							symboloutput = 0;
+							kFinal = 0;
+							jFinal = 0;						
+							k = 93;				
 						}else{					
 							k = k+1;					
 						}				
 				    }
-			    	j = 92;			
+			    	j = 93;			
 			    	}else{
 					j=j+1;			
 				}
@@ -68,25 +78,29 @@ var symbol = [' ', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', '
 		while(i <= inputLength){
 			j = 0;
 			//This fires once for every possible character in the key field 
-			while(j < 92){
+			while(j < 93){
 				k = 0;
 				if (key.substring(i,i+1) == symbol[j]){				
 					var jFinal = j;	
 					//This fires once for every 			
-					while(k < 92){										
-						if(input.substring(i,i+1) == symbol[k]){						
+					while(k < 93){
+						 if(input.substring(i,i+1) == symbol[k]){						
 							var kFinal = k;						
 							var output = kFinal - jFinal;						
-							if(output < 0){output=output+92}
-							var symboloutput = symbol[output];
-							console.log(symboloutput);
-							document.getElementById("output").innerHTML+=symboloutput;						
-							k = 92;				
+							if(output < 0){output=output+93}
+							var symboloutput = printSymbol[output];
+							//console.log(symboloutput);
+							document.getElementById("output").innerHTML+=symboloutput;
+							output = 0;
+							symboloutput = 0;
+							kFinal = 0;
+							jFinal = 0;						
+							k = 93;				
 						}else{					
 							k = k+1;					
 						}				
 				    }
-			    	j = 92;			
+			    	j = 93;			
 			    	}else{
 					j=j+1;			
 				}
@@ -95,4 +109,12 @@ var symbol = [' ', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', '
 	    	i= i+1;
 		} 
 
+	}
+
+	function debug(){
+		var i = 0;
+		while(i < 94){
+			console.log(i+" "+symbol[i])
+		i++
+	    }
 	}
